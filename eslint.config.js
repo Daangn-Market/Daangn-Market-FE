0f9e2aac9d412622b7022793cf85ddc5a58eafc4
gyuhochoime <cjg1999@naver.com>
2025-03-13 18:11:25 +0900

    setting: eslint, prettier, stylelint 설치 및 설정 파일 추가

--- eslint.config.js
+++ eslint.config.js
@@ -7,22 +7,45 @@ import tseslint from 'typescript-eslint'
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended,
+      'plugin:prettier/recommended'
+    ],
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: globals.browser
     },
     plugins: {
       'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      'react-refresh': reactRefresh
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
-        { allowConstantExport: true },
+        { allowConstantExport: true }
       ],
-    },
-  },
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y/no-static-element-interactions': 'off',
+      'react/require-default-props': 'off',
+      'jsx-a11y/no-noninteractive-element-interactions': 'off',
+      'react/button-has-type': 'off',
+      'react/jsx-no-useless-fragment': 'off',
+      'import/prefer-default-export': 'off',
+      'react-hooks/exhaustive-deps': 'off',
+      'consistent-return': 'off',
+      'react/no-array-index-key': 'off',
+      'no-await-in-loop': 'off',
+      'no-plusplus': 'off',
+      'no-param-reassign': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      'prefer-destructuring': 'off',
+      '@typescript-eslint/no-shadow': 'off',
+      'jsx-a11y/label-has-associated-control': 'off'
+    }
+  }
 )
